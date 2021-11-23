9550d4b9e10874e40967bbaa9e5154c475871efa
Yoruhubuki <50508196+Yoruhubuki@users.noreply.github.com>
2021-11-24 00:41:55 +0900

    画面操作での求人追加機能を実装

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Job from "../views/Job.vue";
+import JobEditor from "../views/JobEditor.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +11,18 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/job/:id",
+    name: "job",
+    component: Job,
+    props: true
+  },
+  {
+    path: "/editor",
+    name: "editor",
+    component: JobEditor,
+    props: true
   },
 ];
 
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,14 +1,34 @@
 <template>
-
   <div>
     <v-container>
       <h1>Job Board</h1>
       <div v-for="job in jobs" :key="job.pk">
-        <p>{{ job.company_name }}: {{ job.job_title }}</p>
+        <h2>
+          <router-link
+            :to="{  name: 'job', params: { id: job.id }}"
+            class="job-link"
+          >{{ job.company_name }}
+          </router-link>
+        </h2>
+
+        <p>{{ job.job_title }}</p>
+  
+        <hr>
+      </div>
+
+      <div class="my-4">
+        <p v-show="loadingJobs">...loading...</p>
+
+        <v-btn
+          v-show="next"
+          @click="getJobs"
+          color='success'
+        >Load More
+        </v-btn>
+
       </div>
     </v-container>
   </div>
-
 </template>
 
 <script>
@@ -19,21 +39,47 @@ export default {
   name: "home",
   data() {
     return {
-      jobs: []
+      jobs: [],
+      next: null,
+      loadingJobs: false
     }
   },
   methods: {
     getJobs() {
       let endpoint = 'api/jobs/'
+      if (this.next) {
+        endpoint = this.next;
+      }
+      this.loadingJobs = true;
       apiService(endpoint).then(data => {
         this.jobs.push(...data.results)
+        this.loadingJobs = false;
+        if (data.next) {
+          this.next = data.next;
+        } else {
+          this.next = null;
+        }
       })
     }
   },
   created() {
     this.getJobs()
-    console.log(this.jobs)
+    document.title = 'Job Board';
   }
 };
 
 </script>
+
+<style scoped>
+
+.job-link {
+  font-weight: bold;
+  color: black;
+  text-decoration: none;
+}
+
+.job-link:hover {
+  color: #41B883;
+}
+
+</style>
